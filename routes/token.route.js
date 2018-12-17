efae1f7f83da8d31853455f5faaa4aeae3a3847c
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-18 00:02:08 +0100

    web3 connection

--- routes/token.route.js
+++ routes/token.route.js
@@ -8,14 +8,14 @@ const router = express.Router();
 let tokenCtrl = new TokenCtrl();
 
 let getEvents = tokenCtrl.getEvents.bind(tokenCtrl);
-let getBalanceHistory = tokenCtrl.getBalanceHistory(tokenCtrl);
+// let getBalanceHistory = tokenCtrl.getBalanceHistory(tokenCtrl);
 
 
-// router.route('/events')
-//     .get(getEvents);
+router.route('/events')
+    .get(getEvents);
 
-router.route('/balance')
-    .get(getBalanceHistory);
+// router.route('/balance')
+//     .get(getBalanceHistory);
 
 
 module.exports = router;
