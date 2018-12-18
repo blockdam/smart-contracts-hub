3762bd9ce2139cf8fde2aabc717cc6eadd651218
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-18 12:02:27 +0100

    web3 connection

--- services/token.service.js
+++ services/token.service.js
@@ -4,6 +4,7 @@ const fs = require('graceful-fs');
 const Promise = require('bluebird');
 const logger = require('./logger.service');
 const config = require('../config/index');
+const moment = require('moment');
 
 /**
  * Service for getting dataset for visualization from the post sections and saving the dataset in a separate file so it can be loaded on the static pages
@@ -37,6 +38,13 @@ class TokenService {
             let history = [];
 
             transfers.forEach((transfer) => {
+
+                let prevBalance = {};
+                prevBalance.date  = moment(transfer.date).subtract(1, 's');
+                prevBalance.totalGrants = totalGrants;
+                prevBalance.ethValue = 5 / totalGrants;
+
+                history.push(prevBalance);
 
                 let balance = {};
                 balance.date = transfer.date;
