d41ba59b99b84761a5e30d936423763d99c01b26
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-19 11:33:34 +0100

    web3 connection

--- services/token.service.js
+++ services/token.service.js
@@ -39,7 +39,7 @@ class TokenService {
 
             transfers.forEach((transfer,i) => {
 
-                // we don't eant a gradual line between two balance records in time .. so we make a stub with previous values just before the chnage
+                // we don't want a gradual line between two balance records in time .. so we make a stub with previous values just before the chnage
                 if (i > 0) {
                     let prevBalance = {};
                     prevBalance.date  = moment(transfer.date).subtract(1, 's');
