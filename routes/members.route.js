e4aeb074dd59377788e429292a8b8daca58aa219
Joera <joeramulders@Joeras-MacBook-Pro.local>
2018-12-20 19:26:11 +0100

    code for ciculation graph

--- routes/members.route.js
+++ routes/members.route.js
@@ -4,8 +4,6 @@ let express = require('express'),
     MemberController = require('../controllers/member.controller');
 
 const router = express.Router();
-
-
 const memberController = new MemberController(); // create instance of instagram
 
 let getAll = memberController.getAll.bind(memberController);
@@ -13,13 +11,10 @@ let findOne = memberController.findOne.bind(memberController);
 
 // CRUD routes
 
-router.route('')
-    .get(getAll)
+router.route('/')
+    .get(getAll);
 
 router.route('/:id')
-    .get(findOne)
-// .post(ctrl.getFeed)
-// .put(update)
-// .delete(del);
+    .get(findOne);
 
 module.exports = router;
