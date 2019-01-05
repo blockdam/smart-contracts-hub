d3319c4a6d4a1424ac8030cd1366141c2e68c278
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-05 15:16:44 +0100

    ie11 fix

--- services/url_metadata.service.js
+++ services/url_metadata.service.js
@@ -8,13 +8,10 @@ const got = require('got');
 
 const metascraper = require('metascraper')([
     require('metascraper-author')(),
-    // require('metascraper-date')(),
-    // require('metascraper-description')(),
-    // require('metascraper-image')(),
-    // require('metascraper-logo')(),
-    // require('metascraper-clearbit-logo')(),
-    // require('metascraper-publisher')(),
-    // require('metascraper-title')(),
+    require('metascraper-date')(),
+    require('metascraper-description')(),
+    require('metascraper-publisher')(),
+    require('metascraper-title')(),
     require('metascraper-url')()
 ])
 
