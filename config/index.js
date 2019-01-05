8fcb0c66d3a3495b82653efc1e818ea43552384e
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-01-05 20:10:04 +0100

    ie11 fix

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ const config = {
     db: 'blockdam',
     addresses : {
         bcdToken : '0xA2F071aFe85e8F3ec51bD9ae5284Bf53204Df1b9',
-        readingList : '0xfc4ba82957df8b1d470afe8529117f502ace11d4'
+        readingList : '0x29fc8f8e2efcaafa85c49c9584c05f7e0c3f73bd'
     },
     latestSyncedBlock : '3545527',
     bcdRatio : 1000000000000000000
