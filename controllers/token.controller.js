0d2b70a2973ece675063994844a04d41b7fb216f
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-02-17 21:12:41 +0100

    get token events

--- controllers/token.controller.js
+++ controllers/token.controller.js
@@ -80,6 +80,8 @@ class TokenController {
 
             self.web3Service.get().then( (web3) => {
 
+                logger.info('skolko?');
+
                 self.tokenContract = new web3.eth.Contract(self.tokenAbi,config.addresses.bcdToken);
                 self.tokenContract.getPastEvents("allEvents", options, function (err, data) {
 
