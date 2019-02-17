1a48023e45be83add244a780a8bd1680556da7c7
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-02-17 13:56:56 +0100

    get token events

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ const config = {
         bcdToken : '0xA2F071aFe85e8F3ec51bD9ae5284Bf53204Df1b9',
         readingList : '0x29fc8f8e2efcaafa85c49c9584c05f7e0c3f73bd'
     },
-    latestSyncedBlock : '3862055',
+    latestSyncedBlock : '3885016',
     bcdRatio : 1000000000000000000
 };
 
