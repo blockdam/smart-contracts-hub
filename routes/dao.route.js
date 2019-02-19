85acf7bb5bb0391562586690843abd23a71bda76
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-02-19 18:00:54 +0100

    dao routes

--- routes/dao.route.js
+++ routes/dao.route.js
@@ -1,11 +1,11 @@
 'use strict';
 
 let express = require('express'),
-    daoCtrl = require('../controllers/dao.controller');
+    DaoCtrl = require('../controllers/dao.controller');
 
 const router = express.Router();
 
-const daoCtrl = new daoCtrl();
+const daoCtrl = new DaoCtrl();
 const handlePermissionsCall = daoCtrl.handlePermissionsCall.bind(daoCtrl);
 
 
