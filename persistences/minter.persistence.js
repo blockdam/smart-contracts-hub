5a7505902da3a1898bdcf0671301a4980233aaaf
Joera <joeramulders@Joeras-MacBook-Pro.local>
2019-02-19 18:40:29 +0100

    dao routes

--- persistences/minter.persistence.js
+++ persistences/minter.persistence.js
@@ -46,7 +46,7 @@ class MinterPersistence {
 
         let self = this,
             query = {
-            ethAddress: '0xB6cA51CA72C689b720235aCA37E579f821FA05EE'
+            ethAddress: permissions.userAddress
             };
 
         return new Promise((resolve, reject) => {
